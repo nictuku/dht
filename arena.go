77fabdf8200c8b301615d3fb489564ba8214b317
Yves Junqueira <yves.junqueira@gmail.com>
2012-11-09 01:47:52 +0100

    Benchmark for the memory arena

--- arena.go
+++ arena.go
@@ -2,12 +2,14 @@ package dht
 
 import "sync"
 
-// arena is a free list of byte slices. After creation, they can be used by
-// calling Pop() but afterwords the caller is responsible for calling Push(),
-// which puts the blocks back in the queue for later usage. The bytes given by
-// Pop() are *not* zeroed so the caller should only read positions that it has
-// written to - that can be done easily by shortening the slice at the right
-// point.  written to.
+// arena is a free list that provides quick access to pre-allocated byte
+// slices, greatly reducing memory churn and effectively disabling GC for these
+// allocations. After the arena is created, a slice of bytes can be used by
+// calling Pop(). The caller is responsible for calling Push(), which puts the
+// blocks back in the queue for later usage. The bytes given by Pop() are *not*
+// zeroed, so the caller should only read positions that it knows to have been
+// overwitten. That can be done by shortening the slice at the right place,
+// based on the count of bytes returned by Write() and similar functions.
 type arena struct {
 	// I could have used a buffered channel to implement the freelist, but
 	// this is probably faster.
---
+++ arena_test.go
@@ -0,0 +1,15 @@
+package dht
+
+import (
+	"testing"
+)
+
+func BenchmarkArena(b *testing.B) {
+	b.StopTimer()
+	a := newArena(1024, 1000)
+	
+	b.StartTimer()
+	for i := 0 ; i < b.N ; i++ {
+		a.Push(a.Pop())
+	}
+}
