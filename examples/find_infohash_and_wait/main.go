1cfa12bb0e236185ede07f7ca641c88ee3e02e93
Yves Junqueira <yves.junqueira@gmail.com>
2012-11-17 09:32:18 +0100

    example/find_infohash_and_wait fixes

--- examples/find_infohash_and_wait/main.go
+++ examples/find_infohash_and_wait/main.go
@@ -1,5 +1,13 @@
-// Runs a node on UDP port 11221 and with a web server for debugging available
-// at http://localhost:8080.
+// Runs a node on UDP port 11221 that attempt to collect 100 peers for an 
+// infohash, then keeps running as a passive DHT node.
+//
+// To collect 100 peers, it usually has to contact some 10k nodes. This process
+// is not instant and should take a minute or two, depending on your network
+// connection.
+//
+// NOTE: The node has full debugging level enabled so you'll see a lot of output
+// on the screen. It also runs a web server that can be used to collect
+// debugging stats from http://localhost:8080/debug/vars.
 package main
 
 import (
@@ -12,28 +20,33 @@ import (
 )
 
 const (
-	httpPort = 8080
+	httpPortTCP = 8080
+	dhtPortUDP  = 11221
 )
 
 func main() {
 	l4g.AddFilter("stdout", l4g.DEBUG, l4g.NewConsoleLogWriter())
 
-	d, err := dht.NewDHTNode(11221, 100, false)
+	d, err := dht.NewDHTNode(dhtPortUDP, 100, false)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	go d.DoDHT()
+	go drainresults(d)
 
 	// Give the DHT some time to "warm-up" its routing table.
 	time.Sleep(5 * time.Second)
 
 	d.PeersRequest("\xd1\xc5\x67\x6a\xe7\xac\x98\xe8\xb1\x9f\x63\x56\x59\x05\x10\x5e\x3c\x4c\x37\xa2", false)
 
-	go func() {
-		for _ = range <-d.PeersRequestResults {
+	http.ListenAndServe(fmt.Sprintf(":%d", httpPortTCP), nil)
+}
 
-		}
-	}()
-	http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
+// drainresults loops, constantly reading any new peer information sent by the
+// DHT node and just ignoring them. We don't care about those :-P.
+func drainresults(n *dht.DHT) {
+	for {
+		<-n.PeersRequestResults
+	}
 }
